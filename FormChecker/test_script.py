a5a593042401bf0125a63a6a5b4193fef5b5748c
egor187 <py.egor.py@gmail.com>
2021-07-24 15:15:19 +0300

    add test cases to test_script.py

--- FormChecker/test_script.py
+++ FormChecker/test_script.py
@@ -5,17 +5,16 @@ URL = "http://127.0.0.1:8000/get_form/"
 
 payload_list = [
     {"email": "b@gmail.com"},
+    {"email": "h@gmail.com"},
     {"email": "a@gmail.com", "text": "Hello"},
     {"text": "ello"},
 ]
 
 
 def main(data) -> None:
-    response_list = list()
     for payload in data:
         response = requests.post(URL, data=payload)
-        response_list += response.json()
-    print(response_list)
+        print(response.text)
 
 
 if __name__ == "__main__":
